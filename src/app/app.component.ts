6101e3d947b0a3af5acd64fdd6594689ea541edd
Louis Ritter <louritter1@gmail.com>
2020-03-26 17:34:11 -0400

    added file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { createWorker } from 'tesseract.js';
 
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,17 +13,35 @@ import { createWorker } from 'tesseract.js';
 export class AppComponent {
   title = 'tesseract.js-angular-app';
   ocrResult = 'Recognizing...';
+  selectedFile: ImageSnippet;
+
   constructor() {
-    this.doOCR();
   }
-  async doOCR() {
+
+  yourName = '';
+
+  processFile(imageInput: any) {
+    const file: File = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any) => {
+
+      this.selectedFile = new ImageSnippet(event.target.result, file);
+      this.doOCR(this.selectedFile.src);
+
+    });
+
+    reader.readAsDataURL(file);
+  }
+
+  async doOCR(url) {
     const worker = createWorker({
       logger: m => console.log(m),
     });
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    const { data: { text } } = await worker.recognize(url);
     this.ocrResult = text;
     console.log(text);
     await worker.terminate();
